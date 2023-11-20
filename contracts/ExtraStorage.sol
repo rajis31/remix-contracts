ebffe98698ee630381a6a859a838ba9721b172d7
rajis31 <rajsolan91@gmail.com>
2023-11-19 17:15:11 -0800

    saving

--- contracts/ExtraStorage.sol
+++ contracts/ExtraStorage.sol
@@ -6,8 +6,6 @@ import "./SimpleStorage.sol";
     21. Use the "is" keyword when you want to extend or inherit from existing contract
     22. To override a function, you have to make the function in the parent contract virtual 
         and then add override keyword in the subcontract.
-    23. 
-
 */
 
 contract ExtraStorage is SimpleStorage {
---
+++ contracts/Fundme.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+    We are going to code 3 features
+        - Get funds from users
+        - Withdraw funds
+        - Set a min. funding value in USD
+    
+    24. There is a value field that contains how much ETH we are sendind
+    25. Every single transaction we send has the following fields:
+            - Nonce: tx count for the account
+            - Gas Price: price of gas per unit (usually in WEI)
+            - Gas Limit
+            - To: Address transaction is sent to
+            - Value: Amout of wei to send
+            - Data: What to send in the transaction
+            - v,r,s: components of the tx signature
+    26. Smart contracts can hold funds. 
+    27. Payable functions in Solidity are functions that let a smart contract accept Ether. 
+    28. use msg.value; to see how much wei someone is sending
+*/
+
+
+contract FundMe {
+
+    function fund() public payable  {
+        // Set min fund amoount in USD
+        // 1. how do we send ETH to this contract?
+        msg.value;
+    }
+    // function withdraw() {}
+
+}
